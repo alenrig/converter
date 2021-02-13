00dd189eca9ef94e3aca4c65ef77e7165d477e1c
Aleksandr Gorokhov <agorokhov94@gmail.com>
2021-02-13 11:41:55 +0300

    Add get all needed files in dir func

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, fucker!")
+// GetSrcInDir - Get all files with needed file type in current dir
+func GetSrcInDir() ([]string, error) {
+	srcFiles := []string{}
+	filesInDir, err := ioutil.ReadDir(".")
+	if err != nil {
+		return nil, err
+	}
+	for _, fileInDir := range filesInDir {
+		if strings.HasSuffix(fileInDir.Name(), fileType) && !fileInDir.IsDir() {
+			srcFiles = append(srcFiles, fileInDir.Name())
+		}
+	}
+
+	return srcFiles, nil
 }
