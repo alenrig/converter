ab926259dd30933ad13f00bec2ace2aff0eaf85f
Aleksandr Gorokhov <agorokhov94@gmail.com>
2021-02-15 10:11:23 +0300

    Add datapoints test

--- main_test.go
+++ main_test.go
@@ -40,7 +40,7 @@ func TestGetName(t *testing.T) {
 	}
 }
 
-func TestCutDatapoints(t *testing.T) {
+func TestCutHeader(t *testing.T) {
 	expected := []string{"133Cs 27Al", "133Cs 69Ga", "133Cs 75As"}
 
 	file, _ := OpenSrcFile(&testPath, "DLT001_Al_10_5.dp_rpc_asc")
@@ -50,5 +50,21 @@ func TestCutDatapoints(t *testing.T) {
 		if v != expected[i] {
 			t.Error(v, expected[i])
 		}
+	}
+}
+
+func TestCutDatapoints(t *testing.T) {
+	expectedFirstLine := "2.40000E-002		1.26501E+002	5.46667E-002		8.71941E+003	8.40000E-002		6.51140E+003"
+	expectedLastLine := "1.69520E+001		1.47737E+001	1.69827E+001		1.77897E+005	1.70200E+001		3.06937E+004"
+
+	file, _ := OpenSrcFile(&testPath, "DLT001_Al_10_5.dp_rpc_asc")
+	_, result := CutDatapoints(file)
+
+	if result[0] != expectedFirstLine {
+		t.Error(result[0], expectedFirstLine)
+	}
+
+	if result[len(result)-1] != expectedLastLine {
+		t.Error(result[len(result)-1], expectedLastLine)
 	}
 }
